4fabff995e896bc3b6a629c4f8103a59abd9adf9
Valentina Korovkina <valentinaaakkk@yandex.ru>
2024-06-03 21:23:12 +0300

    verstka forgot_pass

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.myapplication.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
